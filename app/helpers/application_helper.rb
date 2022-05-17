0983b14eb7c4f54a2b2fdba972ef003d670a6560
seiichi39 <jhnnw708@gmail.com>
2022-05-17 09:44:57 +0000

    “ルーティングの設定、タイトル表示の変更”

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def full_title(page_name = "")
+    base_title = "AttendanceApp"
+    if page_name.empty?
+      base_titile
+    else
+      page_name + " | " + base_title
+    end
+  end
+  
 end
