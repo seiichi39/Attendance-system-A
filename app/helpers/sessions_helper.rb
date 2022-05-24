ef25c31506a41319814826d4d875d2c4ba19c152
seiichi39 <jhnnw708@gmail.com>
2022-05-24 12:22:46 +0000

    “認可機能の設定”

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -39,7 +39,12 @@ module SessionsHelper
     end
   end
   
-  #現在ログイン中のユーザーがいればtrue、そうでなければfalseを返す。
+  # 渡されたユーザーがログイン済みのユーザーであればtrueを返す。
+  def current_user?(user)
+    user == current_user
+  end
+  
+  # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返す。
   def logged_in?
     !current_user.nil?
   end
