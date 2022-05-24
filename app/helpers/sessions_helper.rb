7bff9929a61b6a02cbf7f693c56716fdd52b2fa6
seiichi39 <jhnnw708@gmail.com>
2022-05-24 12:56:03 +0000

    “フレンドリーファオーワード機能の設定”

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -49,4 +49,15 @@ module SessionsHelper
     !current_user.nil?
   end
   
+  # 記憶しているURL（またはデフォルトURL）にリダイレクトする。
+  def redirect_back_or(default_url)
+    redirect_to(session[:forwarding_url] || default_url)
+    session.delete(:forwarding_url)
+  end
+  
+  # アクセスしようとしたURLを記憶する。
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+  
 end
