dd6492cec994d65bc7837a98bc576391ffcab164
seiichi39 <jhnnw708@gmail.com>
2022-05-24 22:56:55 +0000

    “departmentカラムの設定”

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,7 +51,7 @@ class UsersController < ApplicationController
     end
   
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
     end
     
     def logged_in_user
