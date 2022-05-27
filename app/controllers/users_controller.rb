49eac4e0128fa67c6b0f4d372c0e045d791a3842
seiichi39 <jhnnw708@gmail.com>
2022-05-27 14:59:50 +0000

    “１ヶ月ごとの表示の切り替え機能の作成”

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,6 @@ class UsersController < ApplicationController
   end
   
   def show
-    @first_day = Date.current.beginning_of_month
-    @last_day = @first_day.end_of_month
   end
   
   def new
