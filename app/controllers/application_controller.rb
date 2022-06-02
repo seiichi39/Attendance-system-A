81e1b31d00759bc01f73a61e7946fe5a6eba77d1
seiichi39 <jhnnw708@gmail.com>
2022-06-02 16:23:41 +0000

    “各種アクセス権限の修正”

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,6 +24,14 @@ class ApplicationController < ActionController::Base
     redirect_to root_url unless current_user.admin?
   end
   
+  def admin_or_correct_user
+    @user = User.find(params[:user_id]) if @user.blank?
+    unless current_user?(@user) || current_user.admin?
+      flash[:danger] = "編集権限がありません"
+      redirect_to(root_url)
+    end
+  end
+  
   def set_one_month 
     @first_day = params[:date].nil? ?
     Date.current.beginning_of_month : params[:date].to_date
--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -48,12 +48,4 @@ class AttendancesController < ApplicationController
       params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
     end
     
-    def admin_or_correct_user
-      @user = User.find(params[:user_id]) if @user.blank?
-      unless current_user?(@user) || current_user.admin?
-        flash[:danger] = "編集権限がありません"
-        redirect_ro(root_url)
-      end
-    end
-    
 end
