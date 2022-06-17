9b65715c458956ba18d921c5903956c4cf6dc2cd
seiichi39 <jhnnw708@gmail.com>
2022-06-17 11:42:31 +0900

    “拠点情報の一覧画面の作成2”

--- app/controllers/bases_controller.rb
+++ app/controllers/bases_controller.rb
@@ -1,10 +1,50 @@
 class BasesController < ApplicationController
-
-  def new
-  end
+  before_action :set_base, only: [:edit, :update, :destroy]
 
   def index
     @bases = Base.all
   end
 
+  def new
+    @base = Base.new
+  end
+ 
+  def create
+    @base = Base.new(base_params)
+    if @base.save
+      flash[:success] = '新規作成に成功しました'
+      redirect_to bases_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @base.update_attributes(base_params)
+      flash[:success] = "拠点情報を更新しました。"
+      redirect_to bases_url
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @base.destroy
+    flash[:success] = "#{@base.base_name}のデータを削除しました。"
+    redirect_to bases_url
+  end 
+
+  private
+  
+  def base_params
+    params.require(:base).permit(:base_number, :base_name, :base_type)
+  end 
+
+  def set_base
+    @base = Base.find(params[:id])
+  end
+  
 end
--- app/models/base.rb
+++ app/models/base.rb
@@ -1,2 +1,7 @@
 class Base < ApplicationRecord
+
+  validates :base_number, presence: true
+  validates :base_name,  presence: true, length: { maximum: 50 }
+  validates :base_type,  presence: true, length: { maximum: 50 }
+
 end
